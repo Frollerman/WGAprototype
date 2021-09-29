b77932217dc0f169b3648b3e37e03679f459bfac
Frollerman <frollerman@yandex.ru>
2021-09-29 23:09:51 +0300

    Added matrix

--- WGAprototype/Buttons.cpp
+++ WGAprototype/Buttons.cpp
@@ -34,21 +34,97 @@ GameButtons::GameButtons(QWidget* pwgt/*= 0*/) : QPushButton(pwgt)
 
     srand(time(NULL));
     int icon = 0;
-    QVector<QString> color;
-    color << "Red" << "Green" << "Blue" << "Black" << "Grey";
 
-    QList<QPushButton *> *listB = new QList<QPushButton *>;
+//    QVector<QString> color;
+//    color << "Red" << "Green" << "Blue" << "Black" << "Grey";
+
+    QVector<QString> imgstr;
+    imgstr << ":/Red.jpg" << ":/Green.jpg" << ":/Blue.jpg" << ":/Black" << ":/Grey";
+
+    QVector<QPixmap *> pix;
+
+    for(int i = 0; i < 5; i++)
+    {
+        QPixmap *p = new QPixmap(imgstr[i]);
+        pix << p;
+        //ix[i]->load(imgstr[i]);
+    }
+
+//    for(int i = 0; i < 5; i++)
+//    {
+//        qDebug() << *pix[i];
+//    }
+
+
+    QVector<QPushButton *> *vecB = new QVector<QPushButton *>;
+
+    int arrB[5][5];
+
     for(int i = 1; i < 6; i++)
     {
         for(int j = 0; j < 5; j++)
         {
             QPushButton *b = new QPushButton();
-            icon = rand() % 5;
-            b->setIcon(QPixmap(":/" + color[icon] + ".jpg"));
+            icon = rand() % 3;
+            arrB[i-1][j] = icon;
+            b->setIcon(*pix[icon]);
+            b->setIconSize(pix[icon]->size());
+            b->setFlat(true);
+            b->setCheckable(true);
             pgrdLayout->addWidget(b, i, j);
-            listB->append(b);
+            vecB->append(b);
         }
     }
+
+    //Black blocks in matrix
+    for (int i = 0; i < 5; i+=2)
+    {
+        arrB[i][1] = arrB[i][3] = 3;
+    }
+
+    //Grey free blocks in matrix
+    for (int i = 1; i < 5; i+=2)
+    {
+        arrB[i][1] = arrB[i][3] = 4;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            qDebug() << arrB[i][j];
+        }
+        qDebug() << Qt::endl;
+    }
+
+    //Creating black blocks
+    for (int i = 1; i < 24; i+=10)
+    {
+        vecB->at(i)->setIcon(*pix[3]);
+        vecB->at(i)->setFlat(true);
+        vecB->at(i)->setCheckable(false);
+
+        vecB->at(i+2)->setIcon(*pix[3]);
+        vecB->at(i+2)->setFlat(true);
+        vecB->at(i+2)->setCheckable(false);
+    }
+
+    //Creating grey free blocks
+    for (int i = 6; i < 24; i+=10)
+    {
+        vecB->at(i)->setIcon(*pix[4]);
+        vecB->at(i)->setFlat(true);
+
+        vecB->at(i+2)->setIcon(*pix[4]);
+        vecB->at(i+2)->setFlat(true);
+    }
+
+//    vecB->at(1)->setIcon(QPixmap(":/Black.jpg"));
+//    vecB->at(3)->setIcon(QPixmap(":/Black.jpg"));
+//    vecB->at(11)->setIcon(QPixmap(":/Black.jpg"));
+//    vecB->at(13)->setIcon(QPixmap(":/Black.jpg"));
+//    vecB->at(21)->setIcon(QPixmap(":/Black.jpg"));
+//    vecB->at(23)->setIcon(QPixmap(":/Black.jpg"));
     //int bCount = 0;
 
 //    for(int i = 0; i < 5; i++)
